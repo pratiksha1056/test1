e6e181fb34601868098b22ac4844ff0d05afe8ae
pratiksha1056 <pratikshad457@gmail.com>
2025-07-05 10:11:31 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main{
   public static void main(String[]args) {
     Addition addObj = new Addition();
     Subtraction subObj= new Subtraction();
-    int num1=10,    num2 b=5;
+    int num1=10,  num2=5;
     int sum =addObj.add(num1,num2);
     int diff = subObj.subtract(num1,num2);
 
